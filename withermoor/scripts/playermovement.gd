db533795c9ba453846d9dbf5a848065b3ef5607e
Thomas Dobratz <tdobratz8149@student.pps.net>
2025-06-06 15:30:41 -0700

    Working on walking - not finished

--- withermoor/scripts/playermovement.gd
+++ withermoor/scripts/playermovement.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -375.0
+const x_acceleration = 100.0
+const max_x_velocity = 300.0
+const jump_velocity = -375.0
 #const MOVE_RIGHT = KEY_A
 
 func _physics_process(delta: float) -> void:
@@ -12,16 +13,25 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	#if Input.is_action_pressed("move_right"):
 		#position.x += SPEED * delta;
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	#Godot default movement
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT):
+		if velocity.x >= max_x_velocity * -1:
+			velocity.x += x_acceleration * -1
+	if Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT):
+		if velocity.x <= max_x_velocity:
+			velocity.x += x_acceleration
 
 	move_and_slide()
