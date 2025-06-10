6224a03de051216600ddf38672bafa657a52d097
Thomas Dobratz <tdobratz8149@student.pps.net>
2025-06-10 13:44:21 -0700

    Walking works, could change values later

--- withermoor/scripts/playermovement.gd
+++ withermoor/scripts/playermovement.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const x_acceleration = 100.0
-const max_x_velocity = 300.0
+const max_x_velocity = 250.0
+const x_acceleration = max_x_velocity / 3
 const jump_velocity = -375.0
 #const MOVE_RIGHT = KEY_A
 
@@ -10,10 +10,10 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+ 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
+		velocity.y = jump_velocity * (delta * 60)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -27,11 +27,21 @@ func _physics_process(delta: float) -> void:
 	#else:
 		#velocity.x = move_toward(velocity.x, 0, SPEED)
 		
+	#left walking
 	if Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT):
 		if velocity.x >= max_x_velocity * -1:
-			velocity.x += x_acceleration * -1
+			velocity.x += x_acceleration * (delta * 60) * -1
+		else:
+			velocity.x = max_x_velocity * (delta * 60) * -1
+	#right walking
 	if Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT):
 		if velocity.x <= max_x_velocity:
-			velocity.x += x_acceleration
+			velocity.x += x_acceleration * (delta * 60)
+		else:
+			velocity.x = max_x_velocity * (delta * 60)
+	
+	#deceleration here
+	if not (Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT) or Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT)):
+		velocity.x /= 1.5 * (delta * 60)
 
 	move_and_slide()
