29a4a8e9bb616dd5252776c5997353b29d9edaad
Thomas Dobratz <tdobratz8149@student.pps.net>
2025-06-12 13:14:06 -0700

    Shrunk the player, and testing

--- withermoor/scripts/playermovement.gd
+++ withermoor/scripts/playermovement.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const max_x_velocity = 250.0
+const max_x_velocity = 150.0
 const x_acceleration = max_x_velocity / 3
 const jump_velocity = -375.0
 #const MOVE_RIGHT = KEY_A
@@ -14,19 +14,10 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity * (delta * 60)
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#if Input.is_action_pressed("move_right"):
-		#position.x += SPEED * delta;
-		
-	#Godot default movement
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		
+	
+	if Input.is_action_just_pressed("jump"):
+		print("jump")
+	
 	#left walking
 	if Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT):
 		if velocity.x >= max_x_velocity * -1:
