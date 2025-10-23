7a9c07314e2f8075993e12610ce62cd22dd31921
Soleo Hess <shess1272@student.pps.net>
2025-10-23 09:53:44 -0700

    starting enemy class and such

--- withermoor/scripts/EnemyTest.gd
+++ withermoor/scripts/EnemyTest.gd
@@ -1,1 +1,16 @@
-extends Node
+extends CharacterBody2D
+class_name TestEnemy
+
+var enemy_health: int
+var has_gravity: bool
+var enemy_contact_damage: int
+var does_contact_damage: bool
+
+func _ready() -> void:
+	pass
+
+
+
+
+#health
+#
