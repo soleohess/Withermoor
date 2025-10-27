24166a9af31c5bc0cc703aed4b25f54f694a282b
Soleo Hess <shess1272@student.pps.net>
2025-10-27 10:03:08 -0700

    Scripting additions for enemies

--- withermoor/scripts/EnemyTest.gd
+++ withermoor/scripts/EnemyTest.gd
@@ -1,14 +1,17 @@
 extends CharacterBody2D
 class_name TestEnemy
 
-var enemy_health: int
-var has_gravity: bool
-var enemy_contact_damage: int
-var does_contact_damage: bool
+@export var enemy_health: int
+@export var has_gravity: bool
+@export var enemy_contact_damage: int
+@export var does_contact_damage: bool
 
 func _ready() -> void:
 	pass
 
+
+func health():
+	enemy_health = 100
 
 
 
---
+++ withermoor/scripts/Hazard.gd
@@ -0,0 +1,7 @@
+extends Node
+class_name hazard
+
+@export var damages_player: bool = false
+@export var damages_enemy: bool = false
+@export var player_damage: int = 0
+@export var enemy_damage: int = 0
