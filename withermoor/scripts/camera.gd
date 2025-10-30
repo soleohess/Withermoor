110c76987ca3ebcb10734a755e8db9d1adbb4644
Thomas Dobratz <144269397+ThomasLD80@users.noreply.github.com>
2025-10-30 08:52:24 -0700

    I fixed the camera movement and the enemy

--- withermoor/scripts/camera.gd
+++ withermoor/scripts/camera.gd
@@ -4,13 +4,9 @@ extends Camera2D
 @export var offsetX: float = 0.0
 @export var offsetY: float = -30.5
 var target_position: Vector2
-@export var following_speed: float = 0.9
+@export var following_speed: float = 0.15
 
 func _physics_process(delta: float) -> void:
 	target_position = Vector2((player.position.x + offsetX), (player.position.y + offsetY))
-	#position.x = (position.x - target_position.x) * following_speed
-	#position.y = (position.y - target_position.y) * following_speed
-	#print(Vector2(position.x, position.y))
+	position = Vector2(((1 - following_speed) * position.x) + (following_speed * target_position.x), ((1 - following_speed) * position.y) + (following_speed * target_position.y))
 	
-	position.x = target_position.x
-	position.y = target_position.y
